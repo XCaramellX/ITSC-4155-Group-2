3663bd02e79e9aa15389af9ffcf03421b0229451
XCaramellX <ndsgirlz@gmail.com>
2023-03-07 12:44:26 -0500

    removed env

--- PromptMe/src/screens/Prompt.js
+++ PromptMe/src/screens/Prompt.js
@@ -31,7 +31,7 @@ export default function Dashboard() {
    
 
   return (
-    <View >
+    <View>
        <StatusBar></StatusBar>
         <ScrollView contentContainerStyle={styles.scrollView} >
         <Header />
